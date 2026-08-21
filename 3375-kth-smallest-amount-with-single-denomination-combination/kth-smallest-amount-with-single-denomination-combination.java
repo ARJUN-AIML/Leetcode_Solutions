21eb035ee64943ec67a136df60e7ae0af2def913
Arjun S <arjunselvaraj0107@gmail.com>
2026-08-22 01:31:12 +0530

    Time: 164 ms (23.08%) | Memory: 43.8 MB (76.92%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -1,77 +1,4 @@
-import java.util.*;
-
 class Solution {
-    public long findKthSmallest(int[] coins, int k) {
-        List<Long> list = new ArrayList<>();
-
-        Arrays.sort(coins);
-
-        for (int coin : coins) {
-            boolean red = false;
-
-            for (long prev : list) {
-                if (coin % prev == 0) {
-                    red = true;
-                    break;
-                }
-            }
-
-            if (!red)
-                list.add((long) coin);
-        }
-
-        long min = list.get(0);
-        long high = (long) k * min;
-
-        int n = list.size();
-        int totalMasks = 1 << n;
-
-        long[] lcms = new long[totalMasks];
-        int[] signs = new int[totalMasks];
-
-        for (int mask = 1; mask < totalMasks; mask++) {
-            long currentLCM = 1;
-            int bits = 0;
-
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    long coin = list.get(i);
-                    long g = gcd(currentLCM, coin);
-
-                    currentLCM /= g;
-
-                    if (currentLCM > high / coin) {
-                        currentLCM = high + 1;
-                        break;
-                    }
-
-                    currentLCM *= coin;
-                    bits++;
-                }
-            }
-
-            lcms[mask] = currentLCM;
-            signs[mask] = (bits % 2 == 1) ? 1 : -1;
-        }
-
-        while (min < high) {
-            long m = min + (high - min) / 2;
-            long count = 0;
-
-            for (int mask = 1; mask < totalMasks; mask++) {
-                if (lcms[mask] <= m)
-                    count += signs[mask] * (m / lcms[mask]);
-            }
-
-            if (count >= k)
-                high = m;
-            else
-                min = m + 1;
-        }
-
-        return min;
-    }
-
     long gcd(long a, long b) {
         while (b != 0) {
             long temp = a % b;
@@ -79,5 +6,47 @@ class Solution {
             b = temp;
         }
         return a;
+    }
+
+    long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+
+    long count(long x, int[] coins) {
+        long ans = 0;
+        int n = coins.length;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long multiple = 1;
+            int cnt = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    multiple = lcm(multiple, coins[i]);
+                    cnt++;
+                    if (multiple > x) break;
+                }
+            }
+
+            if (multiple > x) continue;
+
+            if (cnt % 2 == 1) ans += x / multiple;
+            else ans -= x / multiple;
+        }
+
+        return ans;
+    }
+
+    public long findKthSmallest(int[] coins, int k) {
+        long low = 1;
+        long high = (long) Arrays.stream(coins).min().getAsInt() * k;
+
+        while (low < high) {
+            long mid = (low + high) / 2;
+            if (count(mid, coins) >= k) high = mid;
+            else low = mid + 1;
+        }
+
+        return low;
     }
 }
